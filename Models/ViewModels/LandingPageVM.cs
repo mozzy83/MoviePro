1fb811d7c71c9275ef03353ed3e2d0d7265b337d
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-09 14:16:32 -0600

    Create LandingPageVM

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,14 @@
+﻿using MoviePro.Models.Database;
+using MoviePro.Models.TMDB;
+
+namespace MoviePro.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
