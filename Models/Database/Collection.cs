3ca38d435ed05ff1b35833577f40553b11b9dc4e
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-09 14:00:22 -0600

    Collection and MovieCollection Models
    Created Collection and MovieCollection Models.
    
    MovieCollection is for the creation of a many to many DB table

---
+++ Models/Database/Collection.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviePro.Models.Database
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Models/Database/Movie.cs
+++ Models/Database/Movie.cs
@@ -8,7 +8,7 @@ namespace MoviePro.Models.Database
     public class Movie
     {
         public int Id { get; set; }
-        public int MovieId { get; set; }
+        public int TMDBMovieId { get; set; }
 
         public string Title { get; set; }
         public string TagLine { get; set; }
