3daeb4408a111657796d7eac1e520d3d80e6e823
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-22 17:18:39 -0600

    Home Index controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviePro.Data;
 using MoviePro.Models;
+using MoviePro.Models.ViewModels;
+using MoviePro.Services;
+using MoviePro.Services.Interfaces;
 using System.Diagnostics;
 
 namespace MoviePro.Controllers
@@ -7,15 +12,33 @@ namespace MoviePro.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IRemoteMovieService _tmdbMovieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IRemoteMovieService tmdbMovieService)
         {
             _logger = logger;
+            _context = context;
+            _tmdbMovieService = tmdbMovieService;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            const int count = 18;
+
+            var data = new LandingPageVM()
+            {
+                CustomCollections = await _context.Collection
+                                .Include(c => c.MovieCollections)
+                .ThenInclude(mc => mc.Movie)
+                                .ToListAsync(),
+                NowPlaying = await _tmdbMovieService.SearchMoviesAsync(Enums.MovieCategory.now_playing, count),
+                Popular = await _tmdbMovieService.SearchMoviesAsync(Enums.MovieCategory.popular, count),
+                TopRated = await _tmdbMovieService.SearchMoviesAsync(Enums.MovieCategory.top_rated, count),
+                Upcoming = await _tmdbMovieService.SearchMoviesAsync(Enums.MovieCategory.upcoming, count)
+            };
+            return View(data);
         }
 
         public IActionResult Privacy()
