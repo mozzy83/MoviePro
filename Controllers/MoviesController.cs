f76f7b60415f885eaa133ae399ffe79bc0fd0452
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-24 22:38:01 -0600

    Movies Controller - Library/CRUD/Details

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
@@ -60,6 +61,186 @@ namespace MoviePro.Controllers
 
             return RedirectToAction("Import");
         }
+
+        public async Task<IActionResult> Library()
+        {
+            var movies = await _context.Movie.ToListAsync();
+            return View(movies);
+        }
+
+        public async Task<IActionResult> Details(int? id, bool local = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie? movie = new();
+            if (local)
+            {
+                //Get the Movie data straight from the DB
+                movie = await _context.Movie.Include(m => m.Cast)
+                                            .Include(m => m.Crew)
+                                            .FirstOrDefaultAsync(m => m.Id == id);
+            }
+            else
+            {
+                //Get the movie data from the TMDB API
+                var movieDetail = await _tmdbMovieService.MovieDetailAsync((int)id);
+                movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+            }
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Local"] = local;
+
+            return View(movie);
+        }
+
+
+
+        // GET: Temp/Create
+        public IActionResult Create()
+        {
+            ViewData["CollectionId"] = new SelectList(_context.Collection, "Id", "Name");
+            return View();
+        }
+
+        // POST: Temp/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TMDBMovieId,Title,TagLine,Overview,RunTime,ReleaseDate,Rating,VoteAverage,Poster,PosterType,Backdrop,BackdropType,TrailerUrl")] Movie movie, int collectionId)
+        {
+            if (ModelState.IsValid)
+            {
+                movie.PosterType = movie.PosterFile?.ContentType;
+                movie.Poster = await _imageService.EncodeImageAsync(movie.PosterFile);
+
+                movie.BackdropType = movie.BackdropFile?.ContentType;
+                movie.Backdrop = await _imageService.EncodeImageAsync(movie.BackdropFile);
+
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+
+                await AddToMovieCollection(movie.Id, collectionId);
+
+                return RedirectToAction("Index", "MovieCollections");
+            }
+            return View(movie);
+        }
+
+        // GET: Temp/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Temp/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TMDBMovieId,Title,TagLine,Overview,RunTime,ReleaseDate,Rating,VoteAverage,Poster,PosterType,Backdrop,BackdropType,TrailerUrl")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (movie.PosterFile is not null)
+                    {
+                        movie.PosterType = movie.PosterFile.ContentType;
+                        movie.Poster = await _imageService.EncodeImageAsync(movie.PosterFile);
+                    }
+                    if(movie.BackdropFile is not null)
+                    {
+                        movie.BackdropType = movie.BackdropFile.ContentType;
+                        movie.Backdrop = await _imageService.EncodeImageAsync(movie.BackdropFile);
+                    }
+
+
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", "Movies", new {id = movie.Id, local = true});
+            }
+            return View(movie);
+        }
+
+        // GET: Temp/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Temp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Movie'  is null.");
+            }
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movie.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Library", "Movies");
+        }
+
+        private bool MovieExists(int id)
+        {
+            return (_context.Movie?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+
+
 
         private async Task AddToMovieCollection(int movieId, string? collectionName)
         {
