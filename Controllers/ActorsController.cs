f95b70733cf5e3d933110b91b568f1828c44f878
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-26 21:03:39 -0600

    Actor Details Init

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -14,7 +14,7 @@ namespace MoviePro.Controllers
             _mappingService = mappingService;
         }
 
-        public async Task<IActionResult> Detail(int id)
+        public async Task<IActionResult> Details(int id)
         {
             var actor = await _tmdbMovieService.ActorDetailAsync(id);
             actor = _mappingService.MapActorDetail(actor);
