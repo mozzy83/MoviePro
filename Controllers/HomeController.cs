b3286038529408fd271598afe051631f8fccca6e
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-04-26 21:11:52 -0600

    Removed Privacy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,10 +42,7 @@ namespace MoviePro.Controllers
             return View(data);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
